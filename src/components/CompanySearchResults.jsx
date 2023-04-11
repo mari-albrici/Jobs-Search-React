da0392a64df1a7a5f75cecc7ec4767c1004d3153
Marianna <marianna.albrici@gmail.com>
2023-04-11 20:20:02 +0200

    Added links and titles

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -2,7 +2,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Job from './Job';
 import { getCompanyAction } from '../redux/actions/';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const CompanySearchResults = () => {
@@ -17,13 +17,17 @@ const CompanySearchResults = () => {
 
 	return (
 		<Container>
-			<Row>
-				<Col>
-					{companyJobs.map((companyJob) => (
-						<Job key={companyJob._id} data={companyJob} />
-					))}
-				</Col>
-			</Row>
+			<Container className="d-flex justify-content-between">
+				<h2>The company's jobs:</h2>
+				<Link to={`/`}>HOME</Link>
+			</Container>
+			{companyJobs.map((companyJob) => (
+				<Row key={companyJob._id}>
+					<Col>
+						<Job data={companyJob} />
+					</Col>
+				</Row>
+			))}
 		</Container>
 	);
 };
